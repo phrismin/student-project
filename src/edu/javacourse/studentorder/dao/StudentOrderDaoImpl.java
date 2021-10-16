7ddf3fa0cc1d5d93c05ff0f5eafee987259ee9e7
Alexey Rudoy <rudenw01@gmail.com>
2021-10-16 15:54:52 +0300

    refactor: setParamsForAdult

--- src/edu/javacourse/studentorder/dao/StudentOrderDaoImpl.java
+++ src/edu/javacourse/studentorder/dao/StudentOrderDaoImpl.java
@@ -35,39 +35,9 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
       statement.setInt(1, StudentOrderStatus.START.ordinal());
       statement.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
 
-      // Husband
-      Adult husband = so.getHusband();
-      statement.setString(3, (husband.getSurName()));
-      statement.setString(4, (husband.getGivenName()));
-      statement.setString(5, (husband.getPatronymic()));
-      statement.setDate(6, Date.valueOf(husband.getDateOfBirth()));
-      statement.setString(7, husband.getPassportSeries());
-      statement.setString(8, husband.getPassportNumber());
-      statement.setDate(9, Date.valueOf(husband.getIssueDate()));
-      statement.setLong(10, husband.getIssueDepartment().getOfficeId());
-      Address husbandAddress = husband.getAddress();
-      statement.setString(11, husbandAddress.getPostIndex());
-      statement.setLong(12, husbandAddress.getStreet().getStreetCode());
-      statement.setString(13, husbandAddress.getBuilding());
-      statement.setString(14, husbandAddress.getBuilding());
-      statement.setString(15, husbandAddress.getApartment());
-
-      // Wife
-      Adult wife = so.getWife();
-      statement.setString(16, (wife.getSurName()));
-      statement.setString(17, (wife.getGivenName()));
-      statement.setString(18, (wife.getPatronymic()));
-      statement.setDate(19, Date.valueOf(wife.getDateOfBirth()));
-      statement.setString(20, wife.getPassportSeries());
-      statement.setString(21, wife.getPassportNumber());
-      statement.setDate(22, Date.valueOf(wife.getIssueDate()));
-      statement.setLong(23, wife.getIssueDepartment().getOfficeId());
-      Address wifeAddress = wife.getAddress();
-      statement.setString(24, wifeAddress.getPostIndex());
-      statement.setLong(25, wifeAddress.getStreet().getStreetCode());
-      statement.setString(26, wifeAddress.getBuilding());
-      statement.setString(27, wifeAddress.getBuilding());
-      statement.setString(28, wifeAddress.getApartment());
+      // Husband and wife
+      setParamsForAdult(statement, 3, so.getHusband());
+      setParamsForAdult(statement, 16, so.getWife());
 
       // Marriage
       statement.setString(29, so.getMarriageCertificateId());
@@ -86,6 +56,23 @@ public class StudentOrderDaoImpl implements StudentOrderDao {
     }
 
     return result;
+  }
+
+  private void setParamsForAdult(PreparedStatement statement, int index, Adult adult) throws SQLException {
+    statement.setString(index++, (adult.getSurName()));
+    statement.setString(index++, (adult.getGivenName()));
+    statement.setString(index++, (adult.getPatronymic()));
+    statement.setDate(index++, Date.valueOf(adult.getDateOfBirth()));
+    statement.setString(index++, adult.getPassportSeries());
+    statement.setString(index++, adult.getPassportNumber());
+    statement.setDate(index++, Date.valueOf(adult.getIssueDate()));
+    statement.setLong(index++, adult.getIssueDepartment().getOfficeId());
+    Address address = adult.getAddress();
+    statement.setString(index++, address.getPostIndex());
+    statement.setLong(index++, address.getStreet().getStreetCode());
+    statement.setString(index++, address.getBuilding());
+    statement.setString(index++, address.getBuilding());
+    statement.setString(index, address.getApartment());
   }
 
   // TODO refactoring: make one method
